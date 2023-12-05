78216befdf147dd433f2b31edc5db05169e6b6fe
hayeon-lee <papepopepe@naver.com>
2023-12-05 16:04:49 +0900

    Initialize Repo

--- src/index.js
+++ src/index.js
@@ -1,23 +1,17 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import App from "./pages/App";
-import { BrowserRouter } from "react-router-dom";
-import { UserContextProvider } from "./context/UserContext";
-import { LoaderContextProvider } from "./context/LoaderContext";
-import { WishContextProvider } from "./context/WishContext";
-import { CommentsContextProvider } from "./context/CommentsContext";
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
+const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <UserContextProvider>
-    <LoaderContextProvider>
-      <WishContextProvider>
-        <CommentsContextProvider>
-          <BrowserRouter>
-            <App />
-          </BrowserRouter>
-        </CommentsContextProvider>
-      </WishContextProvider>
-    </LoaderContextProvider>
-  </UserContextProvider>
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
 );
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
